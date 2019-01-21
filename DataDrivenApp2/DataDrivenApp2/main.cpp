7f370dc28f2d75677316dfb93594f9675d059cad
345391 <william.sandow17@bathspa.ac.uk>
2019-01-21 14:47:03 +0000

    DataApp code done

--- DataDrivenApp2/DataDrivenApp2/main.cpp
+++ DataDrivenApp2/DataDrivenApp2/main.cpp
@@ -14,13 +14,16 @@ void printParis(ifstream &infile);
 void printDreamworks(ifstream &infile);
 void printUber(ifstream &infile);
 void printGaming(ifstream &infile);
+void countGameOfThrones(ifstream &infile);
+void countDogs(ifstream &infile);
+void countMusicIn2010(ifstream &infile);
 
 //main function-------------------------------------------------------------------------------------------
 int main() {
 	//instantiating
 	ifstream infile;
-	infile.open("sampleTweets.csv");
 	
+
 	//Printing a line to signify the beginning of the program
 	cout << "            Welcome to the program            " << endl;
 
@@ -35,69 +38,82 @@ int main() {
 //menu function-------------------------------------------------------------------------------------------
 void menu(ifstream &infile) {
 	//Creating a variable that will hold the value that the user chooses
-	char userOption;
+	int userOption;
 
-	//Printing the menu to the console
-	cout << "=================================================================" << endl;
-	cout << "|            Please pick an option            |" << endl;
-	cout << "|                                |" << endl;
-	cout << "| 1 Total number of tweets                    |" << endl;
-	cout << "| 2 Total number of tweets containing the word money        |" << endl;
-	cout << "| 3 Total number of tweets containing the word politics    |" << endl;
-	cout << "| 4 All the tweets containing the word Paris            |" << endl;
-	cout << "| 5 All the tweets containing the word Dreamworks        |" << endl;
-	cout << "| 6 All the tweets containing the word Uber            |" << endl;
-	cout << "| 7 All the tweets containing the word Gaming            |" << endl;
-	cout << "| 8                                |" << endl;
-	cout << "| 9                                |" << endl;
-	cout << "| 10                              |" << endl;
-	cout << "| 11 End the program                        |" << endl;
-	cout << "=================================================================" << endl;
-	cin >> userOption;
+	while (true) {
 
-	//Switch statement to lead to the function that corresponds with whichever option the user picks
-	switch (userOption) {
-	case 1:
-		countTotal(infile);
-		break;
-	case 2:
-		countMoney(infile);
-		break;
-	case 3:
-		countPolitics(infile);
-		break;
-	case 4:
-		printParis(infile);
-		break;
-	case 5:
-		printDreamworks(infile);
-		break;
-	case 6:
-		printUber(infile);
-		break;
-	case 7:
-		printGaming(infile);
-		break;
-	case 8:
-		cout << "Option is invalid for the time being" << endl;
-		menu(infile);
-		break;
-	case 9:
-		cout << "Option is invalid for the time being" << endl;
-		menu(infile);
-		break;
-	case 10:
-		cout << "Option is invalid for the time being" << endl;
-		menu(infile);
-		break;
-	case 11:
-		break;
-	default:
-		cout << "Invalid option" << endl;
-		menu(infile);
+
+
+		//Printing the menu to the console
+		cout << "=================================================================" << endl;
+		cout << "|            Please pick an option           " << endl;
+		cout << "|                                |" << endl;
+		cout << "| 1. Total number of tweets     |" << endl;
+		cout << "| 2. Total number of tweets containing the word money |" << endl;
+		cout << "| 3. Total number of tweets containing the word politics|" << endl;
+		cout << "| 4. All the tweets containing the word Paris  |" << endl;
+		cout << "| 5. All the tweets containing the word Dreamworks  |" << endl;
+		cout << "| 6. All the tweets containing the word Uber  |" << endl;
+		cout << "| 7. All the tweets containing the word Gaming |" << endl;
+		cout << "| 8. Total number of tweets containing the word GameOfThrones and print out the tweets   |" << endl;
+		cout << "| 9. Total number of twets containing Dogs, and their tweets    |" << endl;
+		cout << "| 10. Total of all of the tweets containing music in 2010    |" << endl;
+		cout << "| 11. End the program   |" << endl;
+		cout << "=================================================================" << endl;
+		cin >> userOption;
+
+		infile.open("sampleTweets.csv");
+		if (userOption == 11) {
+			break;
+		}
+		//Switch statement to lead to the function that corresponds with whichever option the user picks
+		switch (userOption) {
+		case 1:
+			countTotal(infile);
+			break;
+		case 2:
+			countMoney(infile);
+			break;
+		case 3:
+			countPolitics(infile);
+			break;
+		case 4:
+			printParis(infile);
+			break;
+		case 5:
+			printDreamworks(infile);
+			break;
+		case 6:
+			printUber(infile);
+			break;
+		case 7:
+			printGaming(infile);
+			break;
+		case 8:
+			countGameOfThrones(infile);
+			break;
+		case 9:
+			countDogs(infile);
+			break;
+		case 10:
+
+			countMusicIn2010(infile);
+			break;
+		
+		default:
+			cout << "Invalid option" << endl;
+
+		}
+
+		
+
+		
+
 	}
 }
 
+	
+void menu(ifstream &infile);
 //countTotal function-------------------------------------------------------------------------------------------
 void countTotal(ifstream &infile) {
 
@@ -123,8 +139,7 @@ void countTotal(ifstream &infile) {
 
 	//Print the value of the counter
 	cout << "The total number of tweets is: " << counter << endl;
-
-
+	infile.close();
 }
 
 //countMoney function-------------------------------------------------------------------------------------------
@@ -155,7 +170,7 @@ void countMoney(ifstream &infile) {
 	}
 	//Print the value of the counter
 	cout << "The number of tweets containing the word " << word << " is: " << counter << endl;
-
+	infile.close();
 }
 
 //countPolitics function-------------------------------------------------------------------------------------------
@@ -186,7 +201,7 @@ void countPolitics(ifstream &infile) {
 	}
 	//Print the value of the counter
 	cout << "The number of tweets containing the word " << word << " is: " << counter << endl;
-
+	infile.close();
 }
 
 //printParis function-------------------------------------------------------------------------------------------
@@ -215,6 +230,7 @@ void printParis(ifstream &infile) {
 			cout << tweet << endl;
 		}
 	}
+	infile.close();
 }
 
 //printDreamworks function-------------------------------------------------------------------------------------------
@@ -243,6 +259,7 @@ void printDreamworks(ifstream &infile) {
 			cout << tweet << endl;
 		}
 	}
+	infile.close();
 }
 
 //printUber function-------------------------------------------------------------------------------------------
@@ -271,7 +288,9 @@ void printUber(ifstream &infile) {
 			cout << tweet << endl;
 		}
 	}
+	infile.close();
 }
+//printGaming function------------------------------------------------------------
 void printGaming(ifstream &infile) {
 
 	//Checking to see if the file is being read
@@ -297,5 +316,104 @@ void printGaming(ifstream &infile) {
 			cout << tweet << endl;
 		}
 	}
+	infile.close();
 }
+//countGameOfThrones function-------------------------------------------------------------------------------------------
+void countGameOfThrones(ifstream &infile) {
 
+	//Checking to see if the file is being read
+	if (infile.good()) {
+		cout << "File is reading..." << endl;
+	}
+	else {
+		cout << "ERROR" << endl;
+	}
+
+	//Creating a variable to hold the tweet, a word string to compare to the tweet string and a counter
+	string tweet;
+	string word = "GameOfThrones";
+	int counter = 0;
+
+	//While is is not (!) the end (.eof()) of the file (infile)
+	//Get the tweet from the file and put it into the tweet variable
+	//If the variable 'word' can be found in the variable 'tweet' within 140 characters
+	//Increase the counter by one
+	while (!infile.eof()) {
+		getline(infile, tweet);
+		if (tweet.find(word) < 140) {
+			counter++;
+			cout << tweet << endl;
+		}
+	}
+	//Print the value of the counter
+	cout << "The number of tweets containing the word " << word << " is: " << counter << endl;
+	infile.close();
+}
+//printDogs function------------------------------------------------------------
+void countDogs(ifstream &infile) {
+
+	//Checking to see if the file is being read
+	if (infile.good()) {
+		cout << "file is reading..." << endl;
+	}
+	else {
+		cout << "ERROR" << endl;
+	}
+
+	//Creating a variable to hold the tweet, a word string to compare to the tweet string and a counter
+	string tweet;
+	string word = "Dogs";
+	int counter = 0;
+
+	//While is is not (!) the end (.eof()) of the file (infile)
+	//Get the tweet from the file and put it into the tweet variable
+	//If the variable 'word' can be found in the variable 'tweet' within 140 characters
+	//Print the tweet to the console
+	while (!infile.eof()) {
+		getline(infile, tweet);
+		if (tweet.find(word) < 140) {
+			counter++;
+			cout << tweet << endl;
+			
+		}
+	}
+	cout << "The number of tweets containing the word " << word << " is: " << counter << endl;
+	infile.close();
+}
+//printDevonandCornwall function------------------------------------------------------------
+void countMusicIn2010(ifstream &infile) {
+
+	//Checking to see if the file is being read
+	if (infile.good()) {
+		cout << "file is reading..." << endl;
+	}
+	else {
+		cout << "ERROR" << endl;
+	}
+
+	//Creating a variable to hold the tweet, a word string to compare to the tweet string and a counter
+	string tweet;
+	string word1 = "2010";
+	int counter = 0;
+
+	
+	string word2 = "Music";
+	
+
+	
+
+	//While is is not (!) the end (.eof()) of the file (infile)
+	//Get the tweet from the file and put it into the tweet variable
+	//If the variable 'word' can be found in the variable 'tweet' within 140 characters
+	//Print the tweet to the console
+	while (!infile.eof()) {
+		getline(infile, tweet);
+		if (tweet.find(word1) < 140 && tweet.find (word2) < 140) {
+			counter++;
+			cout << tweet << endl;
+
+		}
+	}
+	cout << "The number of tweets containing the word " << word1 + " " + word2 << " is: " << counter << endl;
+	infile.close();
+}
